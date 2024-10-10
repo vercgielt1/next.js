587d96283767c7307872164a2aee224ddde619c9
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2024-10-10 14:52:49 -0700

    run CI for PPR + DIO

--- packages/next/src/server/config-shared.ts
+++ packages/next/src/server/config-shared.ts
@@ -1065,7 +1065,7 @@ export const defaultConfig: NextConfig = {
     parallelServerCompiles: false,
     parallelServerBuildTraces: false,
     ppr:
-      // TODO: remove once we've made PPR default
+      // TODO: remove once we've made PPR the default
       // If we're testing, and the `__NEXT_EXPERIMENTAL_PPR` environment variable
       // has been set to `true`, enable the experimental PPR feature so long as it
       // wasn't explicitly disabled in the config.
@@ -1074,7 +1074,7 @@ export const defaultConfig: NextConfig = {
         process.env.__NEXT_EXPERIMENTAL_PPR === 'true'
       ),
     pprFallbacks:
-      // TODO: remove once we've made PPR default
+      // TODO: remove once we've made PPR the default
       // If we're testing, and the `__NEXT_EXPERIMENTAL_PPR` environment variable
       // has been set to `true`, enable the experimental PPR feature so long as it
       // wasn't explicitly disabled in the config.
@@ -1097,7 +1097,15 @@ export const defaultConfig: NextConfig = {
     serverComponentsHmrCache: true,
     staticGenerationMaxConcurrency: 8,
     staticGenerationMinPagesPerWorker: 25,
-    dynamicIO: false,
+    dynamicIO:
+      // TODO: remove once we've made PPR the default
+      // If we're testing, and the `__NEXT_EXPERIMENTAL_PPR` environment variable
+      // has been set to `true`, enable the experimental PPR feature so long as it
+      // wasn't explicitly disabled in the config.
+      !!(
+        process.env.__NEXT_TEST_MODE &&
+        process.env.__NEXT_EXPERIMENTAL_DIO === 'true'
+      ),
   },
   bundlePagesRouterDependencies: false,
 }
